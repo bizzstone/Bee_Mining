3bd232c870a41214dc9914adc96283b5d2a1d1e5
bizzs <bizzs@BIZZ>
2026-07-06 20:15:16 +0700

    config

---
+++ src/eco.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: "Bee-Miner",
+      script: "index.cjs",
+    },
+    {
+      name: "Bee-Miner-1",
+      script: "index1.cjs",
+    },
+    {
+      name: "Bee-Miner-2",
+      script: "index2.cjs",
+    },
+    {
+      name: "Bee-Miner-3",
+      script: "index3.cjs",
+    },
+    {
+      name: "Bee-Miner-4",
+      script: "index4.cjs",
+    },
+    {
+      name: "Bee-Miner-5",
+      script: "index5.cjs",
+    }
+  ]
+};
