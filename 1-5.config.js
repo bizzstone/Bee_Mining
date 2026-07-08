3076ae7a6f45e97621b0d7d7ca9f4684b0a0315a
bizzs <bizzs@BIZZ>
2026-07-08 20:50:58 +0700

    batch worker

--- 1-5.config.js
+++ 1-5.config.js
@@ -4,7 +4,6 @@ module.exports = {
     { name: "mine-2", script: "index.cjs", args: "2", restart_delay: 5000 },
     { name: "mine-3", script: "index.cjs", args: "3", restart_delay: 5000 },
     { name: "mine-4", script: "index.cjs", args: "4", restart_delay: 5000 },
-    { name: "mine-5", script: "index.cjs", args: "5", restart_delay: 5000 },
-    { name: "mine-6", script: "index.cjs", args: "6", restart_delay: 5000 }
+    { name: "mine-5", script: "index.cjs", args: "5", restart_delay: 5000 }
   ]
 };
---
+++ 11-15.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps: [
+    { name: "mine-11", script: "index.cjs", args: "11", restart_delay: 5000 },
+    { name: "mine-12", script: "index.cjs", args: "12", restart_delay: 5000 },
+    { name: "mine-13", script: "index.cjs", args: "13", restart_delay: 5000 },
+    { name: "mine-14", script: "index.cjs", args: "14", restart_delay: 5000 },
+    { name: "mine-15", script: "index.cjs", args: "15", restart_delay: 5000 }
+  ]
+};
