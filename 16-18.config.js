e91901b123ad30db6c7e803878af50be65d418de
bizzs <bizzs@BIZZ>
2026-07-16 20:40:35 +0700

    new  Worker

---
+++ 16-18.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apps: [
+    { name: "mine-16", script: "index.cjs", args: "16", restart_delay: 5000 },
+    { name: "mine-17", script: "index.cjs", args: "17", restart_delay: 5000 },
+    { name: "mine-18", script: "index.cjs", args: "18", restart_delay: 5000 }
+  ]
+};
