80456cde0596b02f68483a3787fa5318a8aeb031
bizzs <bizzs@BIZZ>
2026-07-19 12:43:45 +0700

    16-18 fix

--- 16-18.config.js
+++ 16-18.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   apps: [
     { name: "mine-16", script: "index.cjs", args: "16", restart_delay: 5000 },
-    { name: "mine-17", script: "index.cjs", args: "17", restart_delay: 5000 },
-    { name: "mine-18", script: "index.cjs", args: "18", restart_delay: 5000 }
+    { name: "mine-17", script: "index.cjs", args: "17", restart_delay: 5000 }
   ]
 };
